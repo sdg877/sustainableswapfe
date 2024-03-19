e6d0ef4c504d4b8d48d562770d12fe22c15c849d
Sdg877 <sdrakegill@gmail.com>
2024-03-19 10:38:21 +0000

    bug fixes

--- src/Components/ItemViewPage.jsx
+++ src/Components/ItemViewPage.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallBack } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import SwapForm from './SwapForm';
