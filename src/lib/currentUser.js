805f38f26e9810546b70c2f66ffcc2f7adda950a
Sdg877 <sdrakegill@gmail.com>
2024-03-19 11:51:28 +0000

    fix for jwt errors

--- src/lib/currentUser.js
+++ src/lib/currentUser.js
@@ -1,7 +1,8 @@
 import { jwtDecode } from 'jwt-decode'
 
-export function currentUser() {
-    const token = localStorage.getItem('access_token')
-    const decodedToken = jwtDecode(token)
-    return decodedToken.user_id
-}
+// export function currentUser() {
+//     const token = localStorage.getItem('access_token')
+//     const decodedToken = jwtDecode(token)
+//     return decodedToken.user_id
+// }
+
