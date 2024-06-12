3542fbe01a9de151cb6548b31708f442d3cb3e7a
Sdg877 <sdrakegill@gmail.com>
2024-06-12 07:51:11 +0100

    fixing deployment issues

--- src/Pages/ItemList.jsx
+++ src/Pages/ItemList.jsx
@@ -1,3 +1,40 @@
+// import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+// import '../App/App.css';
+
+// function ItemList() {
+//   const [items, setItems] = useState([]);
+
+//   useEffect(() => {
+//     axios.get(`${process.env.REACT_APP_BACKEND_URL}/items/`)
+//       .then(response => {
+//         setItems(response.data);
+//       })
+//       .catch(error => {
+//         console.error('Error fetching items:', error);
+//       });
+//   }, []);
+//   if (!items) return null;
+
+//   return (
+//     <div className="centered-item-card-container">
+//       <div className="card-container">
+//         {items.map(item => (
+//           <div className="item-card" key={item.id}> 
+//             <Link to={`/items/${item.id}`}>
+//               <h2>{item.item_title}</h2>
+//             </Link>
+//             <p>{item.item_description}</p>
+//           </div> 
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ItemList;
+
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -5,17 +42,29 @@ import '../App/App.css';
 
 function ItemList() {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_BACKEND_URL}/items/`)
-      .then(response => {
+    const fetchItems = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/items/`);
         setItems(response.data);
-      })
-      .catch(error => {
+      } catch (error) {
         console.error('Error fetching items:', error);
-      });
+        setError('Error fetching items');
+      }
+    };
+
+    fetchItems();
   }, []);
-  if (!items) return null;
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (items.length === 0) {
+    return <div>No items found</div>;
+  }
 
   return (
     <div className="centered-item-card-container">
